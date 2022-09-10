0fb23d6ad2b9a3a22294a0115b49bcf087c7003c
Roland <roland.grager@gmail.com>
2022-09-10 16:23:50 +0200

    Update environment variables.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 
 from flask import Flask, render_template, redirect, url_for, flash, abort
@@ -14,7 +15,7 @@ from flask_gravatar import Gravatar
 
 # initialize the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -29,7 +30,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # create database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
